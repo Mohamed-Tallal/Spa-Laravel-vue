399e6001de315894c33f594368bb96081b980d94
Mohamed Tallal <68516727+Mohamed-Tallal@users.noreply.github.com>
2021-11-16 09:47:45 +0200

    last commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,17 +11,16 @@ Vue.component('pagination', require('laravel-vue-pagination'));
 
 import Myheader from  './components/MyHeader'
 import routes from "./routes";
+
 const router = new VueRouter({
     mode:'history',
     routes
-  })
-
+})
 
  const app = new Vue({
     el: '#hello',
     //routers,
     router,
-
     components :{
         Myheader
     }
